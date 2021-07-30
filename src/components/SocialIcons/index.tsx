1c027e001c5029b0a0c8b422240d07d8b025ac72
rafaeldellaquila <rafadellaquila@gmail.com>
2021-07-29 23:53:20 -0300

    :lipstick: new social session

---
+++ src/components/SocialIcons/index.tsx
@@ -0,0 +1,40 @@
+import * as S from './styles'
+import { Instagram as InstagramIcon } from '@styled-icons/entypo-social/Instagram'
+import { Github as GithubIcon } from '@styled-icons/entypo-social/Github'
+import { Linkedin as LinkedinIcon } from '@styled-icons/entypo-social/Linkedin'
+import { Twitter as TwitterIcon } from '@styled-icons/entypo-social/Twitter'
+
+export type SocialIconsProps = {
+  withBefore?: boolean
+}
+
+const SocialIcons = ({ withBefore = true }: SocialIconsProps) => (
+  <S.Wrapper withBefore={withBefore}>
+    <a
+      href="https://github.com/rafaeldellaquila"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <GithubIcon aria-label="ícone do github" />
+    </a>
+    <a href="https://instagram.com/rfldllql" target="_blank" rel="noreferrer">
+      <InstagramIcon aria-label="ícone do instagram" />
+    </a>
+    <a
+      href="https://www.linkedin.com/in/rafaeldellaquila/"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <LinkedinIcon aria-label="ícone do linkedin" />
+    </a>
+    <a
+      href="http://www.twitter.com/rfldllql"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <TwitterIcon aria-label="ícone do twitter" />
+    </a>
+  </S.Wrapper>
+)
+
+export default SocialIcons
---
+++ src/components/SocialIcons/styles.ts
@@ -0,0 +1,35 @@
+import styled, { css, DefaultTheme } from 'styled-components'
+import media from 'styled-media-query'
+import { SocialIconsProps } from '.'
+const wrapperModifiers = {
+  beforeStyle: (theme: DefaultTheme) => css`
+    content: ' ';
+    background-color: ${theme.colors.primary};
+    display: inline-block;
+    width: ${theme.sizes.xhuge};
+    height: ${theme.sizes.xsmall};
+    margin-right: ${theme.sizes.large};
+    border-radius: ${theme.border.smallRadio};
+
+    ${media.lessThan('huge')`
+      width: ${theme.sizes.huge};
+      `}
+  `
+}
+
+export const Wrapper = styled.div<SocialIconsProps>`
+  ${({ theme, withBefore }) => css`
+    display: flex;
+    align-items: center;
+    &::before {
+      ${!!withBefore && wrapperModifiers.beforeStyle(theme)}
+    }
+    a {
+      margin-right: ${theme.sizes.large};
+    }
+    svg {
+      color: ${theme.colors.primary};
+      width: ${theme.sizes.large};
+    }
+  `}
+`
