1de0d5b0050cf333ddc70b6aae93dafda462746b
rafaeldellaquila <rafael@dellaquila.dev>
2024-06-17 23:45:49 -0300

    :coffin: remove old code for the new website version

--- README.md
+++
@@ -1,3 +0,0 @@
-## Getting Started
-
-This is my first website where can you find more information about me, make with love and Next.js. (Complete Readme soon)
--- src/graphql/client.ts
+++
@@ -1,9 +0,0 @@
-import { GraphQLClient } from 'graphql-request'
-
-const client = new GraphQLClient('https://api.github.com/graphql', {
-  headers: {
-    authorization: `Bearer ${process.env.AUTH_0}`
-  }
-})
-
-export default client
