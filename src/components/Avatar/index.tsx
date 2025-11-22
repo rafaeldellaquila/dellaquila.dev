c887037b42eaec96e6718eb2588161c455e28972
rafaeldellaquila <rafael@dellaquila.dev>
2025-11-21 22:48:17 -0300

    removing site and put a redirect

--- src/components/Avatar/index.tsx
+++
@@ -1,7 +0,0 @@
-import Image from 'next/image'
-import AvatarImg from '@public/images/avatar.jpg'
-const Avatar = () => (
-  <Image src={AvatarImg} width="340px" height="350px" alt="Uma foto minha" />
-)
-
-export default Avatar
